8da2aa1bcd9777ca6f6de8002447d5713869f995
Greg Peterson <gpeterson2@gmail.com>
2011-06-02 18:02:58 -0500

    Add tests.
    Although I've been having difficulty actually running them due to
    import issues.

---
+++ test/test_tokenize.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import unittest
+
+from japanese_tokenizer.tokenizer import tokenize
+
+class TestTokenize(unittest.TestCase):
+    def test_tokenize(self):
+        # Ore no Imouto ga Konna ni Kawaii Wake ga Nai
+        text = u'\u4ffa\u306e\u59b9\u304c\u3053\u3093\u306a\u306b\u53ef\u611b\u3044\u308f\u3051\u304c\u306a\u3044'
+        expected = [
+            u'\u4ffa',
+            u'\u306e',
+            u'\u59b9',
+            u'\u304c\u3053\u3093\u306a\u306b',
+            u'\u53ef\u611b',
+            u'\u3044\u308f\u3051\u304c\u306a\u3044',
+        ]
+
+        result = list(tokenize(text))
+
+        self.assertEqual(expected, result)
+
+if __name__ == '__main__':
+    unittest.main()
