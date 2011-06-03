857245fe83b28ccb12ae91cfd462cc4c7675ea2a
Greg Peterson <gpeterson2@gmail.com>
2011-06-02 19:38:24 -0500

    Update main to use argparse.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import argparse
 import codecs
 import os
 import sys
@@ -17,64 +18,86 @@ def get_word_count(words):
         except KeyError:
             unique_words[word] = 1
 
-    for word, count in sorted(unique_words.items(), key= lambda x: -x[1]):
-        # might want to remove single kana values, as they should just be word breaks...
-        #if not remove_single_kana and len(word) > 1 or get_char_type(word) == 'KANJI':    
-
-        # TODO really should eliminate things prior to this point
+    # TODO - probably want to remove non-kanji single character results as
+    # they aren't "words"
+    key = lambda x: -x[1]
+    for word, count in sorted(unique_words.items(), key=key):
+        # TODO - empty words should have already been removed at this point,
+        #  right?
         if word:
             yield (count, word)
 
 def main():
-    # get the arg file name
-    args = sys.argv
+    parser = argparse.ArgumentParser(description='Tokenize Japanese text.')
+    parser.add_argument('-i', '--input', metavar='file', required=True,
+         dest='input_path', help='An input file or directory')
+    parser.add_argument('-o', '--output', metavar='file', required=False,
+         dest='output_path',
+         help='An output file. If not supplied will write to standard output.')
+    parser.add_argument('-e', '--encoding', metavar='encoding',
+         dest='encoding', required=False, help='The file encoding if not utf-8')
+    parser.add_argument('-w', '--include-count', required=False,
+         action='store_true', dest='include_count',
+         help='Include the instance count of each word')
+    parser.add_argument('-v', '--verbose', required=False,
+         action='store_true', dest='verbose',
+         help='Prints verbose output')
 
-    # TODO - add an arg parser
+    args = parser.parse_args()
 
-    if len(args) == 1:
-        print('Need to specify a file encoding')
-        sys.exit(1)
+    # TODO - most of this code count be moved into functions, and then resused
+    # if for example a GUI is added.
 
-    path = args[1]
-    outpath = 'output.txt'
+    # set encoding if applicable
+    encoding = args.encoding
 
-    encoding = ''
-    if len(args) > 2:
-        encoding = args[2]
-
-    if not encoding:
+    if not args.encoding:
         encoding = 'utf-8'
 
-    # open file (as utf-8?)
-    # the test file wasn't utf-8. It may just be how I saved it, but it didn't
-    # work, so yeah, this needs some work.
-
-    #encoding = 'UTF-8'
-    #encoding = 'EUC-JP'
+    # grab file list
+    path = args.input_path
 
     paths = []
     if os.path.isfile(path):
         paths.append(path)
-
     else:
         for path, dirs, files in os.walk(path):
             paths += [os.path.join(path, f) for f in files]
-        
+
+    # Read through the file(s)
     words = []
+    verbose_output = args.verbose
     for path in paths:
         with codecs.open(path, 'r', encoding=encoding, errors='ignore') as f:
-            print(u'Reading file: {0}'.format(path))
+            if verbose_output:
+                print(u'Reading file: {0}'.format(path))
+
             text = f.read()
 
             words += list(tokenize(text))
-            print(u'Finished file: {0}'.format(path))
 
-    output = codecs.open(outpath, 'w', encoding='utf-8')
+            if verbose_output:
+                print(u'Finished file: {0}'.format(path))
+
+
+    # Finally output the words
+    include_count = args.include_count
+    output_path = args.output_path
+
+    if not output_path:
+        # TODO - will fail miserably if console can't handle Unicode
+        output = sys.stdout
+    else:
+        output = codecs.open(output_path, 'w', encoding='utf-8')
+
+    # Grab count anyway, as it will remove duplicates
     for (count, word) in get_word_count(words):
         try:
             encoded = word.encode('utf-8')
-            output.write(u'{0}:\t{1}\n'.format(count, word))
-            print(u'{0}:\t{1}'.format(count, word))
+            if include_count:
+                output.write(u'{0}:\t{1}\n'.format(count, word))
+            else:
+                output.write(u'{0}\n'.format(word))
         except Exception as e:
             print(e)
 
